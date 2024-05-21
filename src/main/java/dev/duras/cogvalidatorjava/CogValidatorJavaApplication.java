c421003ebe9b8c4ddd373799a36cbaa0b48a39bc
batuhan.duras <batugane@gmail.com>
2024-05-21 14:45:30 +0300

    fix: replace the use of System.out by a logger

--- src/main/java/dev/duras/cogvalidatorjava/CogValidatorJavaApplication.java
+++ src/main/java/dev/duras/cogvalidatorjava/CogValidatorJavaApplication.java
@@ -1,6 +1,8 @@
 package dev.duras.cogvalidatorjava;
 
 import dev.duras.cogvalidatorjava.service.CogValidationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class CogValidatorJavaApplication {
+    private static final Logger logger = LoggerFactory.getLogger(CogValidatorJavaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CogValidatorJavaApplication.class, args);
@@ -19,18 +22,17 @@ public class CogValidatorJavaApplication {
     public CommandLineRunner commandLineRunner(CogValidationService validationService) {
         return args -> {
             if (args.length < 1) {
-                System.out.println("Usage: java -jar cog-validator-java-0.0.1.jar <file_path>");
+                logger.error("Usage: java -jar cog-validator-java-0.0.1.jar <file_path>");
                 System.exit(1);
             }
-
             String filePath = args[0];
-
             try {
                 String result = validationService.validateGeoTIFF(filePath);
+                logger.info("Validation result: {}", result);
                 System.out.println(result);
                 System.exit(1);
             } catch (IOException e) {
-                System.err.println("Error validating Cloud Optimized GeoTIFF: " + e.getMessage());
+                logger.error("Error validating Cloud Optimized GeoTIFF: {}", e.getMessage());
                 System.exit(0);
             }
         };
