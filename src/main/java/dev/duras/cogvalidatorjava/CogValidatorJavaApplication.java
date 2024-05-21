f9c98cb15eb582757a7ee2cfd5fcdeed88ab1b74
batuhan.duras <batugane@gmail.com>
2024-05-21 13:41:37 +0300

    initial commit

---
+++ src/main/java/dev/duras/cogvalidatorjava/CogValidatorJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.duras.cogvalidatorjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogValidatorJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogValidatorJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/duras/cogvalidatorjava/CogValidatorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.duras.cogvalidatorjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogValidatorJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
